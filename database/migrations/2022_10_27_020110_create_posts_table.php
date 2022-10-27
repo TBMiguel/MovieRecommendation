3761d766b5dd856111aca099a22a90c02e807887
TBMiguel <miguelmpjr@gmail.com>
2022-10-26 23:38:02 -0300

    Criando arquivos de Models e Migrations

---
+++ database/migrations/2022_10_27_020110_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('user_id');
+
+            $table->string('name'); //nome do filme
+            $table->integer('year'); //Ano publicação
+            $table->string('image');
+            $table->string('classification'); //genero do filme/série
+            $table->string('recommendation'); //recomendado sim ou não
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
