7b2211939840791d2f960e9248e6f47d1b68f1d2
TBMiguel <miguelmpjr@gmail.com>
2022-10-27 23:01:09 -0300

    Adicionando delete nos posts

--- resources/views/livewire/show-user-posts.blade.php
+++ resources/views/livewire/show-user-posts.blade.php
@@ -1,23 +1,3 @@
-@if($edit)
-<form method="POST" enctype="multipart/form-data" wire:submit.prevent="atualizar($postId)">
-    <div class="container-sm card" id="formcreate">
-        <div class="form-group">
-            <label for="name">Titulo do Livro</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Harry Potter" wire:model="name" required> 
-            <label for="year">Ano</label>
-            <input type="number" class="form-control" id="year" name="year" placeholder="2000" wire:model="year" required> 
-            <label for="classification">Gênero</label>
-            <input type="text" class="form-control" id="classification" name="classification" placeholder="Aventura" wire:model="classification" required>
-            <label for="image" class="form-label">Insira uma url da imagem do filme/livro</label>
-            <input class="form-control" type="text" id="image" name="image" wire:model="image" required> 
-        </div>
-        <div class="button">
-            <input type="submit" class="btn btn-warning" value="Atualizar"> 
-        </div>
-    </div>
-</form>
-@endif
-
 @foreach($posts as $post)
     <div class="container-sm card mb-4">
         <div class="row g-0">
